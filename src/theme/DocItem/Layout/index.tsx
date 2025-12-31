f4023fe1eda7a0e75c67f3994b26c637c781a466
imehc <richohealthy@hotmail.com>
2025-12-31 14:59:14 +0800

    feat: 添加toast

--- src/theme/DocItem/Layout/index.tsx
+++ src/theme/DocItem/Layout/index.tsx
@@ -2,10 +2,12 @@ import GiscusComments from "@site/src/components/GiscusComments";
 import type { Props } from "@theme/DocItem/Layout";
 import OriginalDocItemLayout from "@theme-original/DocItem/Layout";
 import type { ReactNode } from "react";
+import { Toaster } from 'sonner';
 
 export default function DocItemLayout(props: Props): ReactNode {
 	return (
 		<>
+			<Toaster position="top-center" richColors />
 			<OriginalDocItemLayout {...props} />
 			{process.env.NODE_ENV === "production" && <GiscusComments />}
 		</>
