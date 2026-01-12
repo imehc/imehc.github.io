537314635abdbcfc86919c694d7980a9a874f3ff
imehc <richohealthy@hotmail.com>
2026-01-12 17:01:06 +0800

    fix: github deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,12 +19,7 @@ const config: Config = {
 	url: "https://imehc.github.io",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/chiyu/",
-
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "imehc", // Usually your GitHub org/user name.
-	projectName: "chiyu", // Usually your repo name.
+	baseUrl: "/",
 
 	onBrokenLinks: "throw",
 
